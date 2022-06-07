6799b6e6ef508a03f1f396147e7b07a79bfe8c2a
chh <houhong.cao@igen-tech.com>
2022-06-07 18:24:22 +0800

    first commit

--- Example/HHTestKit/ViewController.swift
+++ Example/HHTestKit/ViewController.swift
@@ -7,12 +7,15 @@
 //
 
 import UIKit
+import HHTestKit
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let test = HHTest()
+        test.showInfo()
     }
 
     override func didReceiveMemoryWarning() {
---
+++ HHTestKit/Classes/HHTest.swift
@@ -0,0 +1,18 @@
+//
+//  HHTest.swift
+//  HHTestKit
+//
+//  Created by caohouhong on 2022/6/6.
+//
+
+import Foundation
+
+public class HHTest {
+    public init() {
+        
+    }
+    
+    public func showInfo() {
+        print("HHTestKit create success!")
+    }
+}
